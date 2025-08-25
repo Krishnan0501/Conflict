f3e028770a78f40e2387e9191f5f79add3336095
krishnan0501 <krishnan5312002@gmail.com>
2025-08-25 11:07:09 +0530

    hello

---
+++ src/com/kris/Example1.java
@@ -0,0 +1,7 @@
+package com.kris;
+
+public class Example1 {
+	public static void main(String[] args) {
+		System.out.println("1");
+	}
+}
---
+++ src/com/kris/Example2.java
@@ -0,0 +1,8 @@
+package com.kris;
+
+public class Example2 {
+	public static void main(String[] args) {
+		System.out.println("2");
+
+	}
+}
